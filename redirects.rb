5123a15eab4e44a0284dfadefef602f9d1eecc2e
Samia Nneji <snneji@vmware.com>
2023-06-26 13:01:04 +0100

    [#181643074] Migration: attempt to fix regex

--- redirects.rb
+++ redirects.rb
@@ -5,6 +5,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirect from docs.pivotal.io/credhub to docs.vmware.com CredHub docs in TAS v4.0
 r301  %r{/credhub/index.html}, "https://docs.vmware.com/en/VMware-Tanzu-Application-Service/4.0/tas-for-vms/credhub-index.html"
-r301  %r{/credhub/(.*)}, "https://docs.vmware.com/en/VMware-Tanzu-Application-Service/4.0/tas-for-vms/$1"
+r301  %r{/credhub/([\w-]*\.html)}, "https://docs.vmware.com/en/VMware-Tanzu-Application-Service/4.0/tas-for-vms/$1"
 r301  %r{/credhub/}, "https://docs.vmware.com/en/VMware-Tanzu-Application-Service/4.0/tas-for-vms/credhub-index.html"
 r301  %r{/credhub}, "https://docs.vmware.com/en/VMware-Tanzu-Application-Service/4.0/tas-for-vms/credhub-index.html"
